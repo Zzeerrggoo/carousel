37201293ec41c9a7ceab3e398544177509959b0c
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-08-11 15:52:21 +0300

    Add source for Carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './components/carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel
+      slides={[
+        {
+          title: 'Most detailed image of the Crab Nebula',
+          description:
+            "This Hubble image gives the most detailed view of the entire Crab Nebula ever. The Crab is among the most interesting and well studied objects in astronomy. This image is the largest image ever taken with Hubble's WFPC2 camera.It was assembled from 24 individual exposures taken with the NASA/ ESA Hubble Space Telescope and is the highest resolution image of the entire Crab Nebula ever made.",
+          src:
+            'https://cdn.spacetelescope.org/archives/images/thumb700x/heic0515a.jpg',
+        },
+        {
+          title: 'Most detailed image of the Crab Nebula',
+          description:
+            "This Hubble image gives the most detailed view of the entire Crab Nebula ever. The Crab is among the most interesting and well studied objects in astronomy. This image is the largest image ever taken with Hubble's WFPC2 camera.It was assembled from 24 individual exposures taken with the NASA/ ESA Hubble Space Telescope and is the highest resolution image of the entire Crab Nebula ever made.",
+          src:
+            'https://cdn.spacetelescope.org/archives/images/thumb700x/heic0515a.jpg',
+        },
+        {
+          title: 'Most detailed image of the Crab Nebula',
+          description:
+            "This Hubble image gives the most detailed view of the entire Crab Nebula ever. The Crab is among the most interesting and well studied objects in astronomy. This image is the largest image ever taken with Hubble's WFPC2 camera.It was assembled from 24 individual exposures taken with the NASA/ ESA Hubble Space Telescope and is the highest resolution image of the entire Crab Nebula ever made.",
+          src:
+            'https://cdn.spacetelescope.org/archives/images/thumb700x/heic0515a.jpg',
+        },
+        {
+          title: 'Most detailed image of the Crab Nebula',
+          description:
+            "This Hubble image gives the most detailed view of the entire Crab Nebula ever. The Crab is among the most interesting and well studied objects in astronomy. This image is the largest image ever taken with Hubble's WFPC2 camera.It was assembled from 24 individual exposures taken with the NASA/ ESA Hubble Space Telescope and is the highest resolution image of the entire Crab Nebula ever made.",
+          src:
+            'https://cdn.spacetelescope.org/archives/images/thumb700x/heic0515a.jpg',
+        },
+        {
+          title: 'Most detailed image of the Crab Nebula',
+          description:
+            "This Hubble image gives the most detailed view of the entire Crab Nebula ever. The Crab is among the most interesting and well studied objects in astronomy. This image is the largest image ever taken with Hubble's WFPC2 camera.It was assembled from 24 individual exposures taken with the NASA/ ESA Hubble Space Telescope and is the highest resolution image of the entire Crab Nebula ever made.",
+          src:
+            'https://cdn.spacetelescope.org/archives/images/thumb700x/heic0515a.jpg',
+        },
+      ]}
+    />
   );
 }
 
---
+++ src/components/carousel/slide/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Slide extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Slide;
