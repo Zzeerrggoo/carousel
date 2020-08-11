b79d9acfe66b56e51f66caaa97416c15d6789440
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-08-11 16:53:11 +0300

    Add structure of Carousel component

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
+import Slide from './slide';
 
 class Carousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      delay: 1000,
+      isPlaying: false,
+      isFullScreen: false,
+      currentIndex: 0,
+    };
+  }
+
+  get getNextIndex() {
+    const { currentIndex } = this.state;
+    const { slides } = this.props;
+
+    return (currentIndex + 1) % slides.length;
+  }
+
+  get getPrevIndex() {
+    const { currentIndex } = this.state;
+    const { slides } = this.props;
+
+    return (currentIndex - 1 + slides.length) % slides.length;
+  }
+
   render() {
-    return <article></article>;
+    const { slides } = this.props;
+    const { currentIndex } = this.state;
+
+    return (
+      <article>
+        <Slide {...slides[this.getPrevIndex]} />
+        <Slide isCurrentSlide={true} {...slides[currentIndex]} />
+        <Slide {...slides[this.getNextIndex]} />
+      </article>
+    );
   }
 }
 
